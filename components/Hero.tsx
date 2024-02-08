d7bf03c2e719a9fac5d32c784da297cf9215682f
wb-keeper <wb-keeper@mail.ru>
2024-02-08 23:46:19 +0300

    styling hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Hero = () => {
-    return <div>Hero</div>
-}
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero_title">
+          Find, book, or rent a car - quickly and easily!
+        </h1>
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Hero;
