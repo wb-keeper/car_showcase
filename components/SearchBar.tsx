dccf15ff8c875232c327cc06507d445f75f95f2b
wb-keeper <wb-keeper@mail.ru>
2024-02-16 23:08:26 +0300

    Working on Combobox component

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { SearchManufacturer } from "@/components/index";
 
 const handleSearch = () => {
@@ -9,10 +9,14 @@ const handleSearch = () => {
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = () => {
+  const [manufacturer, setManufacturer] = useState("");
   return (
     <form className="searchbar" onSubmit={handleSearch}>
       <div className="searchbar__item">
-        <SearchManufacturer />
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
       </div>
     </form>
   );
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,26 @@
+"use client";
+
 import { FC } from "react";
+import { Combobox, Transition } from "@headlessui/react";
 
-interface SearchManufacturerProps {}
+interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
 
-const SearchManufacturer: FC<SearchManufacturerProps> = () => {
-  return <div></div>;
+const SearchManufacturer: FC<SearchManufacturerProps> = ({
+  manufacturer,
+  setManufacturer,
+}) => {
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]"></Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
