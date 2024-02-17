458c08949bf82382b02d7d5140019e3352954fe0
wb-keeper <wb-keeper@mail.ru>
2024-02-17 23:00:51 +0300

    Working on filter manufactures

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { FC } from "react";
+import { FC, Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { manufacturers } from "@/constants";
 
 interface SearchManufacturerProps {
   manufacturer: string;
@@ -12,11 +14,44 @@ const SearchManufacturer: FC<SearchManufacturerProps> = ({
   manufacturer,
   setManufacturer,
 }) => {
+  const [query, setQuery] = useState("");
+  const filteredManufactures =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, "")),
+        );
   return (
     <div className="search-manufacturer">
       <Combobox>
         <div className="relative w-full">
-          <Combobox.Button className="absolute top-[14px]"></Combobox.Button>
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              alt="car logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Volkswagen"
+            displayValue={(manufacturer: string) => manufacturer}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery(``)}
+          >
+            <Combobox.Options></Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
