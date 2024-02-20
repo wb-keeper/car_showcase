3530a89b115c44480b0976c331f6220213dfe252
wb-keeper <wb-keeper@mail.ru>
2024-02-20 23:34:52 +0300

    woring on cardetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { FC, Fragment } from "react";
-import { Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 
 interface CarDetailsProps {
   isOpen: boolean;
@@ -10,7 +11,36 @@ interface CarDetailsProps {
 const CarDetails: FC<CarDetailsProps> = ({ isOpen, closeModal, car }) => {
   return (
     <>
-      <Transition appear show={isOpen} as={Fragment}></Transition>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="fixed inset-0 bg-black bg-opacity-25" />
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </>
   );
 };
